c8d1ecc75cae053734c2fa9e845fdaafa2de895f
Maria Nery <mnvnery@gmail.com>
2024-06-19 12:14:36 +0100

    first

--- app/offline/page.tsx
+++ app/offline/page.tsx
@@ -3,10 +3,7 @@ import Image from "next/image";
 export default function Offline() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <video width="600" controls playsInline muted>
-                <source src="/ocelot_2.mp4" type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
+           <div>hi</div>
         </main>
     );
 }
